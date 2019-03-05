628d59ccd6291e425722d1f56775f1e3d5a9dd53
Liyan <layyim@yahoo.com.sg>
2019-03-05 10:06:35 +0800

    Tab Layout Setup

--- app/src/main/java/com/example/aly/instagramcloneassignment/SocialMediaActivity.java
+++ app/src/main/java/com/example/aly/instagramcloneassignment/SocialMediaActivity.java
@@ -3,27 +3,36 @@ package com.example.aly.instagramcloneassignment;
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
+import android.support.design.widget.TabLayout;
+import android.support.v4.view.ViewPager;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.view.View;
 
-public class SocialMediaActivity extends AppCompatActivity {
+public class SocialMediaActivity extends AppCompatActivity
+{
+    private android.support.v7.widget.Toolbar toolbar;
+    private ViewPager viewPager;
+    private TabLayout tabLayout;
+    private TabAdapter tabAdapter;
 
     @Override
-    protected void onCreate(Bundle savedInstanceState) {
+    protected void onCreate(Bundle savedInstanceState)
+    {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_social_media);
-        Toolbar toolbar = findViewById(R.id.toolbar);
+
+        setTitle("Social Media App");
+
+        toolbar = findViewById(R.id.myToolbar);
         setSupportActionBar(toolbar);
 
-        FloatingActionButton fab = findViewById(R.id.fab);
-        fab.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-                        .setAction("Action", null).show();
-            }
-        });
+        viewPager = findViewById(R.id.viewPager);
+        tabAdapter = new TabAdapter(getSupportFragmentManager());
+        viewPager.setAdapter(tabAdapter);
+
+        tabLayout = findViewById(R.id.tabLayout);
+        tabLayout.setupWithViewPager(viewPager, false);
     }
 
 }
